f1b4f6dfee68d9cd2d2d79014b7b3b6103cd5c87
Damir Zhumatayev <adidukik@gmail.com>
2024-08-18 13:06:31 +0500

    add header image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
 export default function App() {
   return (
-    <div className='text-3xl font-bold text-center bg-red-500'>Hello World</div>
+    <>
+      <header className='header-image flex items-center justify-center mt-4'>
+        <img src='header.png' width={101} height={24} />
+      </header>
+      <div className='text-3xl font-bold text-center bg-red-500'>
+        Hello World
+      </div>
+    </>
   );
 }
