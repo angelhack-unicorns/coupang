a12463d39c31826ea95d3448d13ff8568c237e59
Damir Zhumatayev <adidukik@gmail.com>
2024-08-23 21:46:49 +0500

    frontend: add isListening:

--- frontend/src/pages/Voice.tsx
+++ frontend/src/pages/Voice.tsx
@@ -1,13 +1,24 @@
 import { Button } from '../components/ui';
 import PauseIcon from '../components/ui/icons/PauseIcon';
+import PlayIcon from '../components/ui/icons/PlayIcon';
 import XMarkIcon from '../components/ui/icons/XMarkIcon';
+import { useState } from 'react';
 
 export default function VoicePage() {
+  const [isListening, setIsListening] = useState(true);
+
   return (
     <div className='flex flex-col justify-between h-full'>
       <div className='flex justify-around w-full absolute bottom-32'>
-        <Button intent='light/dark' shape='circle' className='w-16 h-16'>
-          <PauseIcon />
+        <Button
+          intent='light/dark'
+          shape='circle'
+          className='w-16 h-16'
+          onPress={() => {
+            setIsListening(!isListening);
+          }}
+        >
+          {isListening ? <PauseIcon /> : <PlayIcon />}
         </Button>
         <Button intent='danger' shape='circle' className='w-16 h-16'>
           <XMarkIcon />
