0db5042555ae91d18e0300f8c6bb315373201a04
Damir Zhumatayev <adidukik@gmail.com>
2024-08-24 13:56:46 +0900

    frontend: add tanstack query

---
+++ frontend/src/api/fetch-categories.ts
@@ -0,0 +1,33 @@
+/* example response
+{
+  "items": [
+    {
+      "name": "Dumbbells",
+      "category": "Strength Training",
+      "reason_for_inclusion": "Dumbbells are versatile and can be used to train multiple parts of the body including arms, shoulders, and chest, making you look bigger and stronger."
+    },
+    {
+      "name": "Barbell with Weights",
+      "category": "Strength Training",
+      "reason_for_inclusion": "Barbells are essential for compound exercises like squats, deadlifts, and bench presses, which work on major muscle groups and enhance overall strength."
+    },
+  ]
+}
+*/
+
+export const categories = {
+  items: [
+    {
+      name: 'Dumbbells',
+      category: 'Strength Training',
+      reason_for_inclusion:
+        'Dumbbells are versatile and can be used to train multiple parts of the body including arms, shoulders, and chest, making you look bigger and stronger.',
+    },
+    {
+      name: 'Barbell with Weights',
+      category: 'Strength Training',
+      reason_for_inclusion:
+        'Barbells are essential for compound exercises like squats, deadlifts, and bench presses, which work on major muscle groups and enhance overall strength.',
+    },
+  ],
+};
